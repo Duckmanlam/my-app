dc15d6d91a0a8440cba649a6173d34dcd3a31313
dinhther-al <ntho70122@gmail.com>
2023-05-25 16:30:48 +0700

    update quiz page

--- src/component/index.jsx
+++ src/component/index.jsx
@@ -1,7 +1,7 @@
 export { default as Home } from './Homepage';
-export { default as Manager } from './Manager';
-export { default as Quiz } from './Quiz';
 export { default as Chat } from './Chat-GPT';
 export { default as Scan } from './Convert';
+export { default as Manager } from './Manager';
+export { default as Quiz } from './Quiz';
 // eslint-disable-next-line react-refresh/only-export-components
 export { default as Product_list } from './Product-list';
